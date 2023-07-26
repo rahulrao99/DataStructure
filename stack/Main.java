73f210b0c0bec7168c2b390d11eb23ed15a5d568
Rahul Rao <131229520+rahulrao99@users.noreply.github.com>
2023-07-27 00:32:00 +0530

    Add files via upload

---
+++ stack/Main.java
@@ -0,0 +1,14 @@
+package com.stack;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stack<Integer> si=new Stack<Integer>(5);
+		System.out.println(si.push(10));
+		System.out.println(si.push(20));
+		System.out.println(si.pop());
+
+	}
+
+}
